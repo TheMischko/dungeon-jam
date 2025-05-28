361d144bc05c9466daa9d50bb92666e10885e193
Miško Dvořák <michal.dvorak999@gmail.com>
2025-05-28 08:22:39 +0200

    Test howler audio streaming.

--- frontend/projects/main/src/app/app.component.ts
+++ frontend/projects/main/src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'main';
+
+  private howler: Howl;
+  playing: boolean = false;
+
+  constructor() {
+    this.howler = new Howl({
+      src: ['lunatic.mp3'],
+      volume: 0.5,
+    });
+  }
+
+  togglePlay() {
+    if (this.playing) {
+      this.howler.pause();
+      this.playing = false;
+      return;
+    }
+    this.howler.play();
+    this.playing = true;
+  }
 }
