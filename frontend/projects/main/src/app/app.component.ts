cfdc9c0a92dc6122b69ab71841e8b75393642342
Miško Dvořák <michal.dvorak999@gmail.com>
2025-05-29 18:18:59 +0200

    Add angular material.

--- frontend/projects/main/src/app/app.component.ts
+++ frontend/projects/main/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Howl } from 'howler';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [MatButton],
 })
 export class AppComponent {
   title = 'main';
