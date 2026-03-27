bb8812d84d6800a3845b3f5d4a7a5393a065fc4e
Miško Dvořák <michal.dvorak999@gmail.com>
2026-03-28 00:26:12 +0100

    Use SmartTable for Tags landing.

--- frontend/projects/main/src/app/modules/tags/pages/tags-page/tags-table/tags-table.component.ts
+++ frontend/projects/main/src/app/modules/tags/pages/tags-page/tags-table/tags-table.component.ts
@@ -3,14 +3,30 @@ import {
   Component,
   computed,
   input,
+  output,
+  TemplateRef,
+  viewChild,
 } from '@angular/core';
 import { TagRow } from '../../../models/tag-row.model';
 import { TableColumnConfiguration, TableTrackByFn } from '../../../../../models/table.model';
-import { TableComponent } from '../../../../../components/table/table.component';
+import { SmartTableComponent } from '../../../../../components/table/smart-table/smart-table.component';
+import {
+  ActionsMenuComponent,
+  ActionsMenuConfig,
+} from '@general/components/display/actions-menu/actions-menu.component';
+import { IconButtonComponent } from '@general/components/buttons/icon-button/icon-button.component';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { actionsIconSet, iconSet } from '@general/icons/icons';
 
 @Component({
   selector: 'app-tags-table',
-  imports: [TableComponent],
+  imports: [
+    SmartTableComponent,
+    ActionsMenuComponent,
+    IconButtonComponent,
+    MatMenuTrigger,
+    MatMenu,
+  ],
   templateUrl: './tags-table.component.html',
   styleUrl: './tags-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,6 +35,33 @@ import { TableComponent } from '../../../../../components/table/table.component'
 export class TagsTableComponent {
   readonly tags = input<TagRow[]>([]);
   readonly loading = input<boolean>(false);
+
+  readonly showTracks = output<TagRow>();
+  readonly editTag = output<TagRow>();
+  readonly deleteTag = output<TagRow>();
+
+  readonly actionsColumnTemplate =
+    viewChild.required<TemplateRef<{ $implicit: TagRow }>>('actionsColumn');
+
+  readonly ActionsIcon = actionsIconSet.ActionsMenu;
+
+  readonly actionsMenuConfig = computed<ActionsMenuConfig<TagRow, unknown>[]>(() => [
+    {
+      text: 'Show tracks',
+      icon: iconSet.TracksIcon,
+      onSelected: (tag) => this.showTracks.emit(tag),
+    },
+    {
+      text: 'Edit',
+      icon: actionsIconSet.EditIcon,
+      onSelected: (tag) => this.editTag.emit(tag),
+    },
+    {
+      text: 'Delete',
+      icon: actionsIconSet.DeleteIcon,
+      onSelected: (tag) => this.deleteTag.emit(tag),
+    },
+  ]);
 
   readonly tableConfig = computed<TableColumnConfiguration<TagRow>>(() => ({
     title: {
@@ -30,6 +73,11 @@ export class TagsTableComponent {
       title: 'Tracks',
       sortable: false,
       width: '120px',
+    },
+    actions: {
+      title: '',
+      template: () => this.actionsColumnTemplate(),
+      width: '70px',
     },
   }));
 
